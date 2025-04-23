637ff3d89e00dcb918235300e1ba850088948024
Asrar Ali <mohdasrar3825@gmail.com>
2025-04-23 17:05:03 +0530

    toggle-functionality-text

--- src/assets/Components/Text.jsx
+++ src/assets/Components/Text.jsx
@@ -11,12 +11,12 @@ function Text() {
       }
   return (
     <div>
-      {/* <button id='details' onClick={togglevisibilty} >
+      <button id='details' onClick={togglevisibilty} >
         {isVisible ? "hide" :"show" } Details
         </button>
-       {isVisible && <p id='para-details' >This is the text that can be shown or hidden!</p>}  */}
-        <input id='Password-feild' type={isVisible ? "text" : "password"  } placeholder='Enter password' /> 
-        <button  id='togglebutton' onClick={togglevisibilty} > {isVisible ? "hide" : "show" } password </button> 
+       {isVisible && <p id='para-details' >This is the text that can be shown or hidden!</p>} 
+        {/* <input id='Password-feild' type={isVisible ? "text" : "password"  } placeholder='Enter password' /> 
+        <button  id='togglebutton' onClick={togglevisibilty} > {isVisible ? "hide" : "show" } password </button>  */}
        
     </div>
   )
